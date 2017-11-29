8d0594c772b234f278fc4b9adbcb045d4404953f
Michiel Rook <mrook@php.net>
2017-11-29 14:51:57 +0100

    Simplify

--- src/main/java/org/demo/configuration/DatabaseConfiguration.java
+++ src/main/java/org/demo/configuration/DatabaseConfiguration.java
@@ -1,10 +1,10 @@
 package org.demo.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -14,12 +14,7 @@ public class DatabaseConfiguration {
 	@Bean
 	public DataSource dataSource(@Value("${jdbc.host}") String host, @Value("${jdbc.port}") int port, @Value("${jdbc.database}") String database,
 								 @Value("${jdbc.username}") String username, @Value("${jdbc.password}") String password) {
-		HikariDataSource dataSource = new HikariDataSource();
-		dataSource.setJdbcUrl(String.format("jdbc:postgresql://%s:%d/%s", host, port, database));
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
-
-		return dataSource;
+		return new DriverManagerDataSource(String.format("jdbc:postgresql://%s:%d/%s", host, port, database), username, password);
 	}
 
 	@Bean
